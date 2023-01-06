57d8edf507c2073549cfb4e8a264e9d590b0931d
IgorPolyatskiy <polyatsky.igor@yandex.ru>
2023-01-06 23:21:40 +0700

    Добавили описание

---
+++ dz_01/Program.cs
@@ -0,0 +1,17 @@
+﻿int a,b,max,min;  
+  
+Console.WriteLine("Введите два числа ");  
+a = int.Parse(Console.ReadLine());  
+b = int.Parse(Console.ReadLine());  
+if (a < b)  
+{  
+    max = b;  
+    min = a;  
+}  
+else  
+{  
+    max = a;  
+    min = b;  
+}  
+Console.WriteLine($"max= {max}");  
+Console.WriteLine($"min= {min}");
