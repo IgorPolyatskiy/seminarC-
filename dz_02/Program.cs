6e3248e548c22757ecd18c063605fb93624fbeb5
IgorPolyatskiy <polyatsky.igor@yandex.ru>
2023-01-07 00:25:50 +0700

    Добавил описание ДЗ

---
+++ dz_02/Program.cs
@@ -0,0 +1,20 @@
+﻿int a,b,c;
+int max = 0;
+
+Console.WriteLine("Введите три числа ");
+a = int.Parse(Console.ReadLine());  
+b = int.Parse(Console.ReadLine()); 
+c = int.Parse(Console.ReadLine());
+
+if(a>b)
+{
+    max=a;
+}
+else
+{
+    max=b;
+}
+if(max<c)max=c;
+
+Console.Write("max= "); 
+Console.Write(max); 
